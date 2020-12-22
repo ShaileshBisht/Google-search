d46e0ca49d4ee1438edb5d0bb97829ba7c0ea5df
shaileh Bisht <shaileshbisht765@gmail.com>
2020-12-23 02:22:51 +0530

    complete home page look

---
+++ src/Home.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
+import Search from "./Search";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_header">
+        <div className="home_headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
+        </div>
+
+        <div className="home_headerRight">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+          <AppsIcon />
+          <Avatar />
+        </div>
+      </div>
+
+      <div className="home_body">
+        <img
+          alt=""
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png"
+        ></img>
+        <div className="home_inputContainer">
+          <Search />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Search.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./Search.css";
+import SearchIcon from "@material-ui/icons/Search";
+import MicIcon from "@material-ui/icons/Mic";
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+
+function Search({ hideButtons = false }) {
+  const [input, setInput] = useState("");
+  const history = useHistory();
+
+  const search = (e) => {
+    e.preventDefault();
+
+    history.push("/search");
+  };
+
+  return (
+    <form className="search">
+      <div className="search_input">
+        <SearchIcon className="search_inputIcon" />
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <MicIcon />
+      </div>
+
+      {!hideButtons ? (
+        <div className="search_buttons">
+          <Button type="submit" onClick={search} variant="outlined">
+            Google Search
+          </Button>
+          <Button variant="outlined">I'm Feeling Lucky</Button>
+        </div>
+      ) : (
+        <div className="search_buttons">
+          <Button
+            className="search_buttonsHidden"
+            type="submit"
+            onClick={search}
+            variant="outlined"
+          >
+            Google Search
+          </Button>
+          <Button className="search_buttonsHidden" variant="outlined">
+            I'm Feeling Lucky
+          </Button>
+        </div>
+      )}
+    </form>
+  );
+}
+
+export default Search;
