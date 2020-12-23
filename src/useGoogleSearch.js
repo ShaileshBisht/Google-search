4144ac5877e95aff326edaf077dc8265d2bf4aae
shaileh Bisht <shaileshbisht765@gmail.com>
2020-12-24 02:00:32 +0530

    complete the project and deployed

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import API_KEY from "./keys";
+require("dotenv").config();
 
 const CONTEXT_KEY = "763b837a38faac1c7";
 
@@ -8,7 +9,7 @@ const useGoogleSearch = (term) => {
 
   useEffect(() => {
     const fetchData = async () => {
-      fetch(
+      await fetch(
         `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
       )
         .then((response) => response.json())
