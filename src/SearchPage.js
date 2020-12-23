356921b797f0c334885b3f8ee4b5803a4cab2444
shaileh Bisht <shaileshbisht765@gmail.com>
2020-12-23 23:50:13 +0530

    complete SearchPage buttons

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -5,6 +5,13 @@ import useGoogleSearch from "./useGoogleSearch";
 import Response from "./response";
 import { Link } from "react-router-dom";
 import Search from "./Search";
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { More } from "@material-ui/icons";
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
@@ -24,6 +31,43 @@ function SearchPage() {
         </Link>
         <div className="searchPage_headerBody">
           <Search hideButtons />
+
+          <div className="searchPage_options">
+            <div className="searchPage_optionsLeft">
+              <div className="searchPage_option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchPage_option">
+                <DescriptionIcon />
+                <Link to="/all">News</Link>
+              </div>
+              <div className="searchPage_option">
+                <ImageIcon />
+                <Link to="/all">Images</Link>
+              </div>
+              <div className="searchPage_option">
+                <LocalOfferIcon />
+                <Link to="/all">Shopping</Link>
+              </div>
+              <div className="searchPage_option">
+                <RoomIcon />
+                <Link to="/all">maps</Link>
+              </div>
+              <div className="searchPage_option">
+                <MoreVertIcon />
+                <Link to="/all">More</Link>
+              </div>
+            </div>
+            <div className="searchPage_optionsRight">
+              <div className="searchPage_option">
+                <Link to="/setting">Setting</Link>
+              </div>
+              <div className="searchPage_option">
+                <Link to="/tools">Tools</Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="searchPage_results"></div>
